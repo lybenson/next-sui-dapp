8a132772cce7b9682d89804e42bc1eb1eb86355c
lybenson <gwanbit@gmail.com>
2023-11-18 17:23:01 +0800

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@nextui-org/react'
+
+export default function Home() {
+  return (
+    <main className='w-full text-center'>
+      <Button color='primary'>asdas</Button>
+    </main>
+  )
+}
